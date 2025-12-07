7fe287a91a2bd5829c01e4cbac1740bca0ce7188
Mayank <vijaymaynk2003@gmail.com>
2025-12-07 20:13:11 +0530

    types Annotaions  and  Inference

---
+++ src/typesInTs.ts
@@ -0,0 +1,11 @@
+let drink = "chai";
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let channelName = "MayankxDev13"
+
+let chaiFlavor:string = "masala chai"
+chaiFlavor = "Ginger tea"
+
+
+let chaiOrder:boolean
